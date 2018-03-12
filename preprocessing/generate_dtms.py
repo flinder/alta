55494cabf268cf4e58180e58ff552e3ba84f74d8
Fridolin Linder <fridolin.linder@gmail.com>
2018-03-12 13:37:56 -0400

    added data set name to dtm filenames

--- preprocessing/generate_dtms.py
+++ preprocessing/generate_dtms.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
         
         dtms = []
         for feature_set in feature_sets:
-            outfname = '_'.join([str(x) for x in feature_set]) + '_dtm.pkl'
+            outfname = data_set_name + '_'.join([str(x) for x in feature_set]) + '_dtm.pkl'
             outpath = os.path.join(DATA_PATH, 'dtms/', outfname)
             if data_set['language'] == 'en':
                 out = english_text_processor.vectorize(text, *feature_set)
