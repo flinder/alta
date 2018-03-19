35d493ec7acb2c2d7bcbdf339f551188ef752ef3
Fridolin Linder <fridolin.linder@gmail.com>
2018-03-19 10:13:34 -0400

    some plots for MPSA presentation

--- analysis/sim_result_analysis.R
+++ analysis/sim_result_analysis.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 #devtools::install_github('flinder/flindR')
 library(flindR)
+library(reshape2)
 
 pe = plot_elements()
 DATA_DIR = "../data/"
@@ -23,7 +24,7 @@ pdat = rbind(active, random)
 ggplot(pdat, aes(x = batch, y = f1, color = group)) +
     facet_wrap(~label) +
     geom_line() +
-    scale_color_manual(values = pe$colors, name = '') +
+   s cale_color_manual(values = pe$colors, name = '') +
     pe$theme    
 
 # Support
@@ -39,3 +40,43 @@ ggplot(pdat, aes(x = support, y = f1, color = group)) +
     geom_line() +
     scale_color_manual(values = pe$colors, name = '') +
     pe$theme    
+
+# ==============================================================================
+# Plots for presentation
+# ==============================================================================
+
+twitter_data = read_csv('../data/annotated_german_refugee_tweets.csv') %>%
+    select(-text) %>%
+    mutate(t_annotation = as.factor(ifelse(annotation == 1, 'relevant', 
+                                         'irrelevant')))
+
+# Demonstrate sparsity of relevant twitter data
+ggplot(twitter_data) +
+    geom_bar(aes(x = t_annotation, fill = t_annotation, color = t_annotation), 
+             alpha = 0.5) + 
+    scale_fill_manual(values = pe$colors, name = "", guide = F) +
+    scale_color_manual(values = pe$colors, name = "", guide = F) +
+    xlab('') + ylab('') +
+    pe$theme
+ggsave('../presentation/figures/twitter_proportion.png', width = pe$p_width,
+       height = 0.7*pe$p_width)
+
+# Demonstrate how many tweets have to be annotated to get X relevant labels
+bs_iter = function(i) {
+    p_pos = mean(sample(twitter_data$annotation, nrow(twitter_data), 
+                        replace = T))
+    return(seq(100, 1000, 10) / p_pos) 
+}
+
+bs_out = sapply(1:1000, bs_iter)
+
+pdat = t(bs_out) %>%
+    melt() %>% tbl_df()
+
+ggplot(pdat) +
+    geom_jitter(aes(x = Var2 * 10 + 100, y = value), size = 1, alpha = 0.01) +
+    xlab("# of positively labeled samples") + 
+    ylab("# of required labeled samples") +
+    scale_y_continuous(labels = scales::comma) +
+    pe$theme
+ggsave('../presentation/figures/required_samples.png')
