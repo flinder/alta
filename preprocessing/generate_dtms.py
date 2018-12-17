b35d4301e825ea0ab3c935b26a6cca73d780d732
flinder <fridolin.linder@gmail.com>
2018-12-17 20:48:03 +0100

    downsample breitbart/wiki in dtm generaion

--- preprocessing/generate_dtms.py
+++ preprocessing/generate_dtms.py
@@ -16,6 +16,8 @@ if __name__ == "__main__":
 
     CONFIG = '../config.yaml'
     DATA_PATH = '../data'
+    MAX_ROWS = 24420
+    SEED = 44042
    
     # load config
     with open(CONFIG) as config_file:
@@ -36,6 +38,9 @@ if __name__ == "__main__":
         data_set = config['data_sets'][data_set_name]
         df = pd.read_csv(os.path.join(DATA_PATH, data_set['fname']))
         df = df.loc[df[data_set['text_col']].notnull(), ]
+        # Down sample the breitbart and wiki datasets to save computation
+        if data_set_name != 'tweets':
+            df = df.sample(MAX_ROWS, random_state=SEED)
         text = df[data_set['text_col']]
 
         i_feature_sets = enumerate(feature_sets)
@@ -47,13 +52,13 @@ if __name__ == "__main__":
             outfname = f'{data_set_name}_{feature_string}_dtm.pkl'
             outpath = os.path.join(DATA_PATH, 'dtms/', outfname)
             if not os.path.isfile(outpath):
-                print(f'Processing feature set {i}: {feature_set}')
                 if data_set['language'] == 'en':
                     out = english_text_processor.vectorize(text, *feature_set)
                 else:
                     out = german_text_processor.vectorize(text, *feature_set)
                 with open(outpath, 'wb') as outfile:
+                    print(f'Processed feature set {i}: {feature_set}, shape: {out.shape}')
                     pickle.dump(out, outfile)
 
-        with Pool(1) as p:
+        with Pool(4) as p:
             p.map(process_feature_set, i_feature_sets)
