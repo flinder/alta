8e09e0e302f9e6e36bb0ee6c8e26d7f87ec608f5
Blake Miller <blakeapm@gmail.com>
2019-01-01 12:22:11 -0500

    only do active margin sims for breitbart. Change for server sims.

--- simulation/breitbart.sh
+++ simulation/breitbart.sh
@@ -3,11 +3,11 @@ for i in {0..49};
 do
     for BALANCE in .01 .05 .1 .3 .5; 
     do
-        for Q in 'margin' 'committee';
+        for MODE in 'active'; 
         do
-            for MODE in 'random' 'active'; 
+            for Q in 'margin';
             do
-                echo Bal: $BALANCE, Iter: $i, Mode: $MODE
+                echo Bal: $BALANCE, Iter: $i, Mode: $MODE, Query Strat.: $Q
                 python3 active_learning_sim.py breitbart --mode $MODE --query_strat $Q --balance $BALANCE --iter $i
             done
         done
